9022b8c6cfbf07eac693639f4359b518fdf890fe
JorgeValdemar <jorge.valdemar.s@gmail.com>
2016-11-02 15:42:09 -0200

    padroes de visual

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,24 @@
+function Main() {
+    var _self = this;
+    
+    _self.scrollTo = function() {
+        $('.scroll-to').click(function() {
+            var scroll = $(this).data().scroll;
+            var $item = $('.scroll-'+scroll);
+            if($item.length > 0) {
+                $('html,body').animate({scrollTop: $item.offset().top},'slow');
+            }
+        });
+    };
+    
+    _self.construtor = function() {
+        _self.scrollTo();
+    };
+    _self.construtor();
+};
 
+
+var main;
+$(function() {
+    main = new Main();
+});
